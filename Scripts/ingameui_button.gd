77a6ee949f836cecbfd75a4cc4e80c62010b6ae1
Mian Abdullah Zahid <66887772+iabdullahzahid@users.noreply.github.com>
2026-03-27 17:34:29 +0500

    test added

--- Scripts/ingameui_button.gd
+++ Scripts/ingameui_button.gd
@@ -4,6 +4,23 @@ extends Button
 var hum_tween: Tween
 
 func _on_mouse_entered():
+	
+	
+	#win scence
+	WinSceneData.pipe_tiles     = 45
+	WinSceneData.peak_pressure  = 2.51
+	WinSceneData.data_collected = 24991
+	WinSceneData.repair_reserve = 3
+	
+	SceneTransition.transition_to("res://Scenes/win_scene.tscn", SceneTransition.Type.ARMOUR)
+	
+	#loose scence
+	#WinSceneData.pipe_tiles     = 33 
+	#WinSceneData.peak_pressure  = 8.4
+	#WinSceneData.data_collected = 12000
+	#WinSceneData.survival_time  = 240.0    
+	#WinSceneData.failure_cause = "PRESSURE OVERLOAD"
+	#SceneTransition.transition_to("res://Scenes/LoseScene.tscn", SceneTransition.Type.ARMOUR)
 	# Create a pulsing effect on the border color
 	hum_tween = create_tween().set_loops()
 	hum_tween.tween_property(hover_style, "border_color", Color("ff00ff"), 0.8)
